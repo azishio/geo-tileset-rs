f611eea44ac0dbd22afd23895c3834da4745df82
azishio <azishio3141@gmail.com>
2024-07-20 21:39:11 +0900

    🚨 deriveを追加

--- src/asset.rs
+++ src/asset.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 /// Metadata about the entire tileset.
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Asset {
     /// The 3D Tiles version. The version defines the JSON schema for the tileset JSON and the base set of tile formats.
